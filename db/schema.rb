15a439bcd39c4957b739776ccdcd80e9d7bde85d
benxta4424 <benxtaben@gmail.com>
2024-12-18 14:40:33 +0200

    adding the location of the events (which i forgot to add in the previous commit)

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_18_123146) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_18_124007) do
   create_table "events", force: :cascade do |t|
     t.string "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "location"
   end
 
   create_table "users", force: :cascade do |t|
