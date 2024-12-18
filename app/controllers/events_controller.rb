c4062cf4727ae686029076b8cbb9c006d9490c0a
benxta4424 <benxtaben@gmail.com>
2024-12-19 00:30:04 +0200

    creating the events for the current user and styling the root page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,23 @@ class EventsController < ApplicationController
   def index
     @events=Event.all
   end
+
+  def new_event_path
+    @event=Event.new
+  end
+
+  def create
+    @event=current_user.build_event(event_params)
+
+    if @event.save
+      redirect_to root
+    else
+      redirect_to new_event_path
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(current_user.id, :date, :name, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "events#index"
   devise_for :users
-  resources :events, only: [ :index ]
+  resources :events, only: [ :index, :new, :create, :show ]
+  resources :users, only: [ :index, :new, :create, :show ]
 end
