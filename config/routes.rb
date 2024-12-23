26ed78069bf987b3d530a4d61529d9d48d6d1091
benxta4424 <benxtaben@gmail.com>
2024-12-23 15:33:32 +0200

    adding the through table and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   resources :events, only: [ :index, :new, :create, :show ]
   resources :users, only: [ :index, :new, :create, :show ]
+  resources :attendaces, only: [ :index, :new, :create, :show ]
 end
